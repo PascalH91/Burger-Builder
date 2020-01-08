d7a6cb10d2e0724d30de612562032250ca4434bc
Pascal Hanke <pascal.hanke@yahoo.de>
2020-01-08 22:16:02 +0100

    change ingredients amount

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Aux from "../../hoc/Aux";
-import styles from "./layout.module.css";
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = props => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={styles.Content}>{props.children}</main>
+      <main className={classes.content}>{props.children}</main>
     </Aux>
   );
 };
