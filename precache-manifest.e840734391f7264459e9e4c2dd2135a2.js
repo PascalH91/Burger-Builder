a0e14c81e5d021b97ef7ed96ca15c62a3e3d23c7
Pascal Hanke <pascal.hanke@yahoo.de>
2020-01-08 18:57:30 +0100

    Updates

---
+++ precache-manifest.e840734391f7264459e9e4c2dd2135a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5e4a50a11fe774d6e598ffd07dbc9a",
+    "url": "/burgerbuilder/index.html"
+  },
+  {
+    "revision": "892ef64639bb1002ba1b",
+    "url": "/burgerbuilder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "43d07ba33499e0757e58",
+    "url": "/burgerbuilder/static/js/2.a0758726.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burgerbuilder/static/js/2.a0758726.chunk.js.LICENSE"
+  },
+  {
+    "revision": "892ef64639bb1002ba1b",
+    "url": "/burgerbuilder/static/js/main.8ef82592.chunk.js"
+  },
+  {
+    "revision": "26d92ac1f04eac46b622",
+    "url": "/burgerbuilder/static/js/runtime-main.46335a2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burgerbuilder/static/media/logo.5d5d9eef.svg"
+  }
+]);
